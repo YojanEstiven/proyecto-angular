8cb5a1adcde7d6633428c36825b35d755ca085bb
yojanestiven1624 <yojanestiv16@gmail.com>
2026-06-03 15:35:33 -0500

    correccion APIKEy

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  groqApiKey: atob('Z3NrX2w4aVcxeEp0Y3ZYMHBtZnF3UVVlV0dyeWIzRlk3MjZPSG8xQ1Z4a2VhMVlBb2JsZXJKYXY=')
+  groqApiKey: atob('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')
 };
